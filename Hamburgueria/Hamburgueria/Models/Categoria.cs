3760d82409187f47de79e7ab070681df5c855896
Welton <weltonrdesigner@gmail.com>
2024-01-27 16:51:00 -0300

    Classe lanche e Categoria

---
+++ Hamburgueria/Hamburgueria/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace Hamburgueria.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+
+    }
+}
